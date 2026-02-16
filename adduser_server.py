a171130ed2e77c0a9002ca6903f27e0d1b651604
27MrMan <gautamkrishus1@gmail.com>
2026-02-16 17:57:02 +0530

    Added encryption for new user authentication still same functionality, rn the auth programs have to be run every time for each user... ill fix that eventually :3

--- adduser_server.py
+++ adduser_server.py
@@ -3,6 +3,27 @@ import socket
 import sys
 from csv import writer
 
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Random import get_random_bytes
+
+key = RSA.generate(2048)
+pkey = key
+public_key = key.publickey()
+pub_key_str = key.publickey().export_key().decode('utf-8')
+
+# Encrypt
+def rsa_encrypt(plaintext, public_key):
+    cipher = PKCS1_OAEP.new(public_key)
+    encrypted = cipher.encrypt(plaintext.encode())
+    return encrypted
+
+# Decrypt
+def rsa_decrypt(ciphertext, private_key):
+    cipher = PKCS1_OAEP.new(private_key)
+    decrypted = cipher.decrypt(ciphertext)
+    return decrypted.decode()
 
 IP = "127.0.0.1" 
 PORT = 2700
@@ -15,19 +36,44 @@ print(f"Listening for clients at {(IP, PORT)}")
 userid_list = []
 epwd_list = []
 
-
+'''
 while True:
     data, address = sock.recvfrom(4096)
 
     print(f"Received {data} from {address}")
 
-    if data.decode()[:3] == "uid":
+    if data.decode() == 'getkey':
+        sock.sendto(pub_key_str.encode(), address)
+    else:
+        data = rsa_decrypt(data, pkey)
+
+    if data[:3] == "uid":
         userid_list.append(data.decode()[3:])
-    if data.decode()[:2] == "pw":
+    if data[:2] == "pw":
         epwd_list.append(data.decode()[2:])
 
     if any(epwd_list) and any(userid_list):
         break
+'''
+#temporary adjustment until i figure out better solution
+data, address = sock.recvfrom(4096)
+
+if data.decode() == 'getkey':
+    sock.sendto(pub_key_str.encode(), address)
+
+data, address = sock.recvfrom(4096)
+data = rsa_decrypt(data, pkey)
+print(data, "added")
+
+if data[:3] == "uid":
+    userid_list.append(data[3:])
+
+data, address = sock.recvfrom(4096)
+data = rsa_decrypt(data, pkey)
+
+if data[:2] == "pw":
+    epwd_list.append(data[2:])
+
 
 message = "Credentials Recieved!"
 
--- clientauth.py
+++ clientauth.py
@@ -3,6 +3,23 @@ import sys
 import hashlib
 import getpass
 
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Random import get_random_bytes
+
+# Encrypt
+def rsa_encrypt(plaintext, public_key):
+    cipher = PKCS1_OAEP.new(public_key)
+    encrypted = cipher.encrypt(plaintext.encode())
+    return encrypted
+
+# Decrypt
+def rsa_decrypt(ciphertext, private_key):
+    cipher = PKCS1_OAEP.new(private_key)
+    decrypted = cipher.decrypt(ciphertext)
+    return decrypted.decode()
+
 
 SERVER_IP = 'yellow-custody.gl.at.ply.gg'
 SERVER_PORT = 46163
@@ -16,15 +33,25 @@ def uid_hash(uid,psw):
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+sock.sendto('getkey'.encode(), (SERVER_IP, SERVER_PORT))
+
+key1, address = sock.recvfrom(4096)
+key1 = RSA.import_key(key1.decode())
+
 userid = input("Create your username ").strip()
 password = getpass.getpass(prompt="Create a password ").strip()
 
 a = uid_hash(userid, password)
 
 userid = 'uid'+userid
-sock.sendto(userid.encode(), (SERVER_IP, SERVER_PORT))
+userid = rsa_encrypt(userid, key1)
+
+sock.sendto(userid, (SERVER_IP, SERVER_PORT))
+
 password = 'pw'+a
-sock.sendto(password.encode(), (SERVER_IP, SERVER_PORT))
+password = rsa_encrypt(password, key1)
+
+sock.sendto(password, (SERVER_IP, SERVER_PORT))
 
 print("credentials sent, awaiting reply...")
 data, address = sock.recvfrom(4096) 
